2d0e2e44f7bf8524d4a4864b5d945e319cae81cd
Miguel Silva <miguelrmsilva@gmail.com>
2022-11-24 15:05:43 +0100

    Corrigido MovementResult era uma classe em vez de um record.

--- src/pt/ulusofona/lp2/deisiJungle/MovementResult.java
+++ src/pt/ulusofona/lp2/deisiJungle/MovementResult.java
@@ -1,15 +1,3 @@
 package pt.ulusofona.lp2.deisiJungle;
 
-class MovementResult {
-    MovementResultCode code;
-    String message;
-
-    public MovementResult(MovementResultCode code) {
-        this.code = code;
-    }
-
-    public MovementResult(MovementResultCode code, String message) {
-        this.code = code;
-        this.message = message;
-    }
-}
+record MovementResult(MovementResultCode code, String message) {}
