a0f6351cb23626d52d89a2c1e2ec0c942341ff5d
Ana Weng <anarita.weng2002.04@gmail.com>
2022-12-31 15:00:35 +0000

    Testes unitários v1.0

--- src/pt/ulusofona/lp2/deisiJungle/TestGameManager.java
+++ src/pt/ulusofona/lp2/deisiJungle/TestGameManager.java
@@ -175,16 +175,12 @@ public class TestGameManager {
         String[][] jogadores = {jogador1, jogador2, jogador3, jogador4};
         String[][] alimentos = {alimento1, alimento2, alimento3, alimento4, alimento5};
 
-
-
         gameManager.createInitialJungle(20, jogadores, alimentos);
 
-        System.out.println(Arrays.deepToString(gameManager.getPlayersInfo()));
         gameManager.moveCurrentPlayer(4,true);
 
-        System.out.println(Arrays.deepToString(gameManager.getPlayersInfo()));
+        Assert.assertEquals(gameManager.getPlayersInfo(),gameManager.getPlayersInfo());
     }
-
 
     @Test
     public void testECarne () {
@@ -209,10 +205,11 @@ public class TestGameManager {
 
         gameManager.createInitialJungle(20, jogadores, alimentos);
 
-        System.out.println(Arrays.deepToString(gameManager.getPlayersInfo()));
+        assertEquals("MovementResult[code=VALID_MOVEMENT, message=null]"
+                ,String.valueOf(gameManager.moveCurrentPlayer(1, true)));
 
-        System.out.println(gameManager.moveCurrentPlayer(1, true));
-        System.out.println(gameManager.moveCurrentPlayer(1, true));
+        assertEquals("MovementResult[code=CAUGHT_FOOD, message=Apanhou Carne]"
+                ,String.valueOf(gameManager.moveCurrentPlayer(1, true)));
 
     }
 
@@ -236,19 +233,22 @@ public class TestGameManager {
         String[][] jogadores = {jogador1, jogador2, jogador3, jogador4};
         String[][] alimentos = {alimento1, alimento2, alimento3, alimento4, alimento5};
 
-
         gameManager.createInitialJungle(20, jogadores, alimentos);
 
-        System.out.println(Arrays.deepToString(gameManager.getPlayersInfo()));
+        Assert.assertEquals("MovementResult[code=CAUGHT_FOOD, message=Apanhou Bananas]",
+                String.valueOf(gameManager.moveCurrentPlayer(1, true)));
 
-        System.out.println(gameManager.moveCurrentPlayer(1, true));
-        System.out.println(gameManager.moveCurrentPlayer(1, true));
+        Assert.assertEquals("MovementResult[code=CAUGHT_FOOD, message=Apanhou Bananas]",
+                String.valueOf(gameManager.moveCurrentPlayer(1, true)));
 
+        Assert.assertEquals("MovementResult[code=CAUGHT_FOOD, message=Apanhou Bananas]",
+                String.valueOf(gameManager.moveCurrentPlayer(1, true)));
 
-        System.out.println(gameManager.moveCurrentPlayer(1, true));
-        System.out.println(gameManager.moveCurrentPlayer(1, true));
+        Assert.assertEquals("MovementResult[code=CAUGHT_FOOD, message=Apanhou Bananas]",
+                String.valueOf(gameManager.moveCurrentPlayer(1, true)));
 
-        System.out.println((Arrays.deepToString(gameManager.getPlayersInfo())));
+        Assert.assertEquals("[[1, João, E, 200, 1..6], [2, Maria, P, 106, 5..6], [3, Manuel, T, 189, 1..3]," +
+                " [5, Pedro, Z, 68, 1..6]]",Arrays.deepToString(gameManager.getPlayersInfo()));
     }
 
     @Test
@@ -271,16 +271,15 @@ public class TestGameManager {
         String[][] jogadores = {jogador1, jogador2, jogador3, jogador4};
         String[][] alimentos = {alimento1, alimento2, alimento3, alimento4, alimento5};
 
-
         gameManager.createInitialJungle(20, jogadores, alimentos);
 
-        System.out.println(Arrays.deepToString(gameManager.getPlayersInfo()));
+        Assert.assertEquals("MovementResult[code=CAUGHT_FOOD, message=Apanhou Cogumelo Magico]"
+                , String.valueOf(gameManager.moveCurrentPlayer(1, true)));
 
-        System.out.println(gameManager.moveCurrentPlayer(1, true));
-        System.out.println(gameManager.moveCurrentPlayer(1, true));
+        Assert.assertEquals("MovementResult[code=CAUGHT_FOOD, message=Apanhou Cogumelo Magico]"
+                , String.valueOf(gameManager.moveCurrentPlayer(1, true)));
 
-
-        System.out.println((Arrays.deepToString(gameManager.getSquareInfo(2))));
+        Assert.assertEquals(gameManager.getSquareInfo(2),gameManager.getSquareInfo(2));
     }
 
     @Test
@@ -341,16 +340,25 @@ public class TestGameManager {
 
         gameManager.createInitialJungle(50, jogadores, alimentos);
         gameManager.moveCurrentPlayer(1, true);
-        System.out.println(gameManager.moveCurrentPlayer(10, true));
+
+        Assert.assertEquals("MovementResult[code=VALID_MOVEMENT, message=null]",
+                String.valueOf(gameManager.moveCurrentPlayer(10, true)));
+
         gameManager.moveCurrentPlayer(1, true);
+
+        Assert.assertEquals("MovementResult[code=VALID_MOVEMENT, message=null]",
+                String.valueOf(gameManager.moveCurrentPlayer(10, true)));
+
         gameManager.moveCurrentPlayer(1, true);
-        System.out.println(gameManager.moveCurrentPlayer(20, true));
 
+        Assert.assertEquals("MovementResult[code=NO_ENERGY, message=null]",
+                String.valueOf(gameManager.moveCurrentPlayer(20, true)));
 
-        System.out.println(Arrays.toString(gameManager.getWinnerInfo()));
+        assertNull(gameManager.getWinnerInfo());
 
-        System.out.println(gameManager.getGameResults());
-
+        Assert.assertEquals("[#1 Maria, Passaro, 11, 10, 0, #2 Pedro, Tarzan, 11, 10, 0, " +
+                        "#3 João, Elefante, 3, 2, 1, #4 Manuel, Tartaruga, 2, 1, 1]",
+                String.valueOf(gameManager.getGameResults()));
     }
 
     @Test
@@ -360,34 +368,124 @@ public class TestGameManager {
         String[] jogador1 = {"1", "João", "P"};
         String[] jogador2 = {"3", "Manuel", "T"};
 
-        String[] alimento1 = {"b", "7"};
-        String[] alimento2 = {"b", "28"};
+        String[] alimento1 = {"b", "18"};
 
 
         String[][] jogadores = {jogador1, jogador2};
-        String[][] alimentos = {alimento1, alimento2};
+        String[][] alimentos = {alimento1};
 
         game.createInitialJungle(45, jogadores, alimentos);
 
-        System.out.println(game.moveCurrentPlayer(6, true));
+        game.moveCurrentPlayer(6, true);
         game.moveCurrentPlayer(0, true);
+        Assert.assertEquals("[[1, João, P, 46, 5..6], [3, Manuel, T, 155, 1..3]]",
+                Arrays.deepToString(game.getPlayersInfo()));
 
-
-        System.out.println(game.moveCurrentPlayer(6, true));
+        game.moveCurrentPlayer(6, true);
         game.moveCurrentPlayer(0, true);
+        Assert.assertEquals("[[1, João, P, 22, 5..6], [3, Manuel, T, 160, 1..3]]",
+                Arrays.deepToString(game.getPlayersInfo()));
 
-        System.out.println(game.moveCurrentPlayer(6, true));
+        game.moveCurrentPlayer(6, true); //não tem energia
         game.moveCurrentPlayer(0, true);
+        Assert.assertEquals("[[1, João, P, 22, 5..6], [3, Manuel, T, 165, 1..3]]",
+                Arrays.deepToString(game.getPlayersInfo()));
 
-        System.out.println(game.moveCurrentPlayer(6, true));
+        game.moveCurrentPlayer(4, true);
         game.moveCurrentPlayer(0, true);
+        Assert.assertEquals("[[1, João, P, 6, 5..6], [3, Manuel, T, 170, 1..3]]",
+                Arrays.deepToString(game.getPlayersInfo()));
 
-        System.out.println(game.moveCurrentPlayer(3, true));
+        Assert.assertEquals("CAUGHT_FOOD",
+                String.valueOf(game.moveCurrentPlayer(1, true).code()));
+
         game.moveCurrentPlayer(0, true);
+        Assert.assertEquals("[[1, João, P, 42, 5..6], [3, Manuel, T, 175, 1..3]]",
+                Arrays.deepToString(game.getPlayersInfo()));
+    }
 
-        System.out.println(Arrays.deepToString(game.getPlayersInfo()));
+    @Test
+    public void testAgua() {
+        int numJogada = 0;
+
+        GameManager gameManager = new GameManager();
+        Agua agua = new Agua();
 
 
+        String[] jogador1 = {"1", "João", "E"};
+        String[] jogador2 = {"3", "Manuel", "T"};
+        String[] jogador3 = {"5", "Pedro", "Z"};
+        String[] jogador4 = {"2", "Maria", "P"};
+
+        String[] alimento1 = {"b", "5"};
+        String[] alimento2 = {"a", "4"};
+        String[] alimento3 = {"c", "7"};
+        String[] alimento4 = {"e", "6"};
+        String[] alimento5 = {"m", "2"};
+
+        String[][] jogadores = {jogador1, jogador2, jogador3, jogador4};
+        String[][] alimentos = {alimento1, alimento2, alimento3, alimento4, alimento5};
+
+        gameManager.createInitialJungle(20, jogadores, alimentos);
+
+        Assert.assertEquals(0,agua.getQuantidadeBananas());
+
+        Assert.assertEquals("Agua : + 15U|20% energia", agua.getToolTip(numJogada));
+
+        Assert.assertEquals(195, agua.calcularEnergia(180,"h", 0, 0));
+
+        Assert.assertEquals(216, agua.calcularEnergia(180,"o", 0, 0));
+
+        Assert.assertEquals(195, agua.calcularEnergia(180,"c", 0, 0));
+
+        Assert.assertEquals(-1, agua.calcularEnergia(180,"l", 0, 0));
+
+        Assert.assertEquals("[Agua]", Arrays.toString(agua.getSaveInfo()));
+    }
+
+    @Test
+    public void testCachoBananas() {
+        int numJogada = 0;
+
+        GameManager gameManager = new GameManager();
+        CachoBananas cachoBananas = new CachoBananas();
+
+
+        String[] jogador1 = {"1", "João", "E"};
+        String[] jogador2 = {"3", "Manuel", "T"};
+        String[] jogador3 = {"5", "Pedro", "Z"};
+        String[] jogador4 = {"2", "Maria", "P"};
+
+        String[] alimento1 = {"b", "5"};
+        String[] alimento2 = {"a", "4"};
+        String[] alimento3 = {"c", "7"};
+        String[] alimento4 = {"e", "6"};
+        String[] alimento5 = {"m", "2"};
+
+        String[][] jogadores = {jogador1, jogador2, jogador3, jogador4};
+        String[][] alimentos = {alimento1, alimento2, alimento3, alimento4, alimento5};
+
+        Assert.assertEquals(3, cachoBananas.getQuantidadeBananas());
+
+        Assert.assertEquals("Bananas : " + cachoBananas.getQuantidadeBananas() + " : + 40 energia",
+                cachoBananas.getToolTip(numJogada));
+
+        assertEquals(-1,cachoBananas.calcularEnergia(180,"l", 0, 0));
+
+        assertEquals(220, cachoBananas.calcularEnergia(180,"h", 0, 0));
+
+        assertEquals(220, cachoBananas.calcularEnergia(180,"o", 0, 0));
+
+        assertEquals(220, cachoBananas.calcularEnergia(180,"c", 0, 0));
+
+        assertEquals(180, cachoBananas.calcularEnergia(180,"h", -1, 0));
+
+        assertEquals(180, cachoBananas.calcularEnergia(180,"h", 2, 0));
+
+        assertEquals(180, cachoBananas.calcularEnergia(180,"o", 3, 0));
+
+        Assert.assertEquals("[Bananas, 0]", Arrays.toString(cachoBananas.getSaveInfo()));
 
     }
+
 }
